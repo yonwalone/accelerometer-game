a251630c42f755a0f73f4616ec383fa709ccc9b7
yonwalone <lukas.zwaller@gmx.de>
2023-05-17 23:37:57 +0200

    feat: add camera to project

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,15 +1,26 @@
+import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
+import Camera from './Camera.js'
+
+let instance = null
 
 export default class Experience {
     constructor(canvas) {
+        // Singleton
+        if (instance) {
+            return instance
+        }
+        instance = this
         // Global access
         window.experience = this
         // Options
-        this.canas = canvas
+        this.canvas = canvas
         // Setup
         this.sizes = new Sizes()
         this.time = new Time()
+        this.scene = new THREE.Scene()
+        this.camera = new Camera()
 
         // Resize event
         this.sizes.on('resize', () => {
@@ -23,10 +34,10 @@ export default class Experience {
     }
 
     resize() {
-
+        this.camera.resize()
     }
 
     update() {
-
+        this.camera.update()
     }
 }
