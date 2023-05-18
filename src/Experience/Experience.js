3bdaca894ec83f274fd6ce16982529c252969598
yonwalone <lukas.zwaller@gmx.de>
2023-05-18 12:29:35 +0200

    feat: create renderer and enable resources

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -2,6 +2,10 @@ import * as THREE from 'three'
 import Sizes from './Utils/Sizes.js'
 import Time from './Utils/Time.js'
 import Camera from './Camera.js'
+import Renderer from './Renderer.js'
+import World from './World/World.js'
+import Resources from './Utils/Resources.js'
+import sources from './sources.js'
 
 let instance = null
 
@@ -20,7 +24,10 @@ export default class Experience {
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.resources = new Resources(sources)
         this.camera = new Camera()
+        this.renderer = new Renderer()
+        this.world = new World()
 
         // Resize event
         this.sizes.on('resize', () => {
@@ -35,9 +42,11 @@ export default class Experience {
 
     resize() {
         this.camera.resize()
+        this.renderer.resize()
     }
 
     update() {
         this.camera.update()
+        this.renderer.update()
     }
 }
